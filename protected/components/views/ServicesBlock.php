0d12ba6d165dbacb51c62b9e5f5ee16e72d8f7a2
Dima <nike@dima.(none)>
2012-02-23 21:38:04 +0400

    comments, design, etc

--- protected/components/views/ServicesBlock.php
+++ protected/components/views/ServicesBlock.php
@@ -1,11 +1,10 @@
 <ul id="slider">
     <? foreach($servs as $serv):?>
     <li>
-        <a href="#">
-            <span class="img"><img src="<?php echo Yii::app()->request->baseUrl; ?>/resources/images/catalog/<?= $serv->id?>.png"
+        <?= CHtml::link('
+            <span class="img"><img src="'.Yii::app()->request->baseUrl.'/resources/images/catalog/'.$serv->id.'.png"
                                    alt=""/></span>
-            <span class="title"><?= $serv->title?></span>
-        </a>
+            <span class="title">'.$serv->title.'</span>', array('item/index', 'id'=>$serv->id));?>
     </li>
     <? endforeach;?>
 </ul>
